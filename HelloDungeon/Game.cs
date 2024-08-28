516bcf666cdc125c6aa0098de535efd04231ec6b
charlie-flynn <charlie.flynn.business@gmail.com>
2024-08-28 10:38:02 -0500

    Initial project upload

---
+++ HelloDungeon/Game.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloDungeon
+{
+    internal class Game
+    {
+        public void Run()
+        {
+            Console.WriteLine("What's your name, adventurer?");
+            string? playerName = Console.ReadLine();
+            while (string.IsNullOrEmpty(playerName))
+            {
+                Console.WriteLine("Sorry, what was that?");
+                    playerName = Console.ReadLine();
+            }
+
+            float playerHealth = 10.0f;
+            float playerMana = 5.0f;
+            int playerGold = 3;
+            string playerAlignment = "Neutral";
+            float playerProximityToNearestLivingSkeleton = 12.0f;
+            bool playerAlive = true;
+
+
+            Console.WriteLine("Hello, " + playerName + "!");
+            Console.WriteLine();
+            Console.WriteLine("Welcome to the dungeon!");
+            Console.WriteLine();
+            Console.WriteLine("Health: " + playerHealth);
+            Console.WriteLine("Mana: " + playerMana);
+            Console.WriteLine("Gold: " + playerGold);
+            Console.WriteLine("Alignment: " + playerAlignment);
+            Console.WriteLine("Alive?: " + playerAlive);
+            Console.WriteLine("Proximity to Nearest Living Skeleton: " + playerProximityToNearestLivingSkeleton + " meters");
+            Console.WriteLine();
+            Console.WriteLine("Gray bricks line the walls of the dungeon,"
+                + " and dust and dread permeate the air.");
+                Console.WriteLine("Two doors stand side by side on the wall in front of you."
+                + " Which do you choose?");
+            Console.WriteLine();
+        }
+    }
+}
